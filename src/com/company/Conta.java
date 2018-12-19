b4fde40aeb88ee9ee0acb22642bdc38b5c10cec2
Edson <edsaraujoc@gmail.com>
2018-12-19 19:49:21 -0200

    Add Metodos em Conta.java

--- src/com/company/Conta.java
+++ src/com/company/Conta.java
@@ -2,9 +2,50 @@ package com.company;
 
 public class Conta {
 
-    private double valor;
+    private double saldo;
     private int agencia;
     private int numero;
     private Cliente titular;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+
+        return false;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
 
 }
